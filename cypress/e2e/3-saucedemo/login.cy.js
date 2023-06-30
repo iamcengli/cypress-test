4cfedd857166be0123a9177db0240e569c205f2a
iamcengli <harrygunawan932@gmail.com>
2023-06-30 15:46:12 +0700

    add failed login scenario

--- cypress/e2e/3-saucedemo/login.cy.js
+++ cypress/e2e/3-saucedemo/login.cy.js
@@ -4,5 +4,17 @@ describe('Login Scenario', () => {
     cy.get('#user-name').type('standard_user')
     cy.get('#password').type('secret_sauce')
     cy.get('[data-test="login-button"]').click()
+    cy.get('.app_logo').should('be.visible')
+    cy.get('.title').should('be.visible')
+  })
+})
+
+describe('Failed Login', () => {
+  it('Login Failed', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('#user-name').type('harry')
+    cy.get('#password').type('password')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').should('be.visible')
   })
 })
